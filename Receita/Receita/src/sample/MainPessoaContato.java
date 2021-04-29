7858946d8c4b45d2170e9fc95945c196a9f53eb0
Marcia Ravadelli <71162341+Lerigou@users.noreply.github.com>
2021-04-29 12:29:05 -0300

    Update MainPessoaContato.java

--- Receita/Receita/src/sample/MainPessoaContato.java
+++ Receita/Receita/src/sample/MainPessoaContato.java
@@ -6,11 +6,13 @@ import sample.model.Pessoa;
 public class MainPessoaContato {
 
     public static void main(String[] args){
+        //Adiciona as informações
         Pessoa pessoa = new Pessoa("Marcia");
         pessoa.setContato("48 9586-8545",
                 "marcia_ravadelli@estudante.sc.senai.br",
                 "xuxu_beleza");
 
+        //Imprime
         System.out.println(pessoa);
 
     }
